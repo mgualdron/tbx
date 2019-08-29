47a6b396c77e5dba1ad953807bc76c0783b6abbd
Miguel Gualdron <dev@gualdron.com>
2019-08-29 12:29:11 -0500

    Added support for -l, -r, and --full to print_table_from_file() and print_table_from_file_multi().

--- src/tbx.c
+++ src/tbx.c
@@ -26,7 +26,11 @@ static char quote = CSV_QUOTE;
 static int ignore_this = 0;
 static int just_pushed_record = 0;
 static DArray *current_record = NULL;
-static int delim_len = 0;
+static int delim_len  = 0;
+static int full_mode  = -1;
+static int line_arg   = 1;
+static int row_arg    = 1;
+static int header_arg = 1;
 
 // The callbacks for CSV processing:
 void cb1 (void *s, size_t len, void *data);
@@ -62,6 +66,12 @@ More than one FILE can be specified.\n\
   -C  --csv              parse CSV files\n\
   -Q, --csv-quote        CSV quoting character (ignored unless --csv)\n\
   -x, --transpose        transpose the output\n\
+  -l, --from-line=NUM    process starting from this line number\n\
+  -r, --row-number=NUM   process this number of rows starting at -l\n\
+  -H, --header           process the first line in the file (header)\n\
+  -A, --add-header       file has no header, so build a generic one (col1  col2  ...)\n\
+  -F, --full             process the whole file (default if not transposing)\n\
+  -h, --help             this help\n\
 ");
     }
 
@@ -70,11 +80,16 @@ More than one FILE can be specified.\n\
 
 
 static struct option long_options[] = {
-    {"csv",       no_argument,       0, 'C'},
-    {"delimiter", required_argument, 0, 'd'},
-    {"csv-quote", required_argument, 0, 'Q'},
-    {"transpose", no_argument,       0, 'x'},
-    {"help",      no_argument,       0, 'h'},
+    {"csv"       , no_argument,       0, 'C'},
+    {"delimiter" , required_argument, 0, 'd'},
+    {"csv-quote" , required_argument, 0, 'Q'},
+    {"transpose" , no_argument,       0, 'x'},
+    {"from-line" , required_argument, 0, 'l'},
+    {"rows"      , required_argument, 0, 'r'},
+    {"add-header", no_argument,       0, 'A'},
+    {"header"    , no_argument,       0, 'H'},
+    {"full"      , no_argument,       0, 'F'},
+    {"help"      , no_argument,       0, 'h'},
     {0, 0, 0, 0}
 };
 
@@ -86,7 +101,7 @@ void chomp(char *s) {
 
 
 /* Print an array of arrays in table form */
-void print_table_from_matrix(DArray *master)
+void print_table_from_matrix(DArray *matrix)
 {
 
     int i, j;
@@ -102,9 +117,9 @@ void print_table_from_matrix(DArray *master)
     /* Change border style */
     ft_set_border_style(table, FT_NICE_STYLE);
 
-    for (i = 0; i < DArray_count(master); i++) {
+    for (i = 0; i < DArray_count(matrix); i++) {
 
-        record = (DArray *)(master->contents[i]);
+        record = (DArray *)(matrix->contents[i]);
 
         if ( DArray_end(record) > 0 ) {
 
@@ -125,29 +140,29 @@ void print_table_from_matrix(DArray *master)
 }
 
 /* Destroy 2-D arrays */
-void master_destroy ( DArray *master )
+void master_destroy ( DArray *matrix )
 {
     int i;
 
     /* Destroy the individual record arrays */
-    for (i = 0; i < DArray_count(master); i++) {
-        DArray *record = DArray_get(master, i);
+    for (i = 0; i < DArray_count(matrix); i++) {
+        DArray *record = DArray_get(matrix, i);
         DArray_destroy(record);
     }
 
-    /* Destroy the master array */
-    DArray_destroy(master);
+    /* Destroy the matrix array */
+    DArray_destroy(matrix);
 }
 
-/* Find the max number of columns in any record of master */
-int DArray_maxcols( DArray *master )
+/* Find the max number of columns in any record of matrix */
+int DArray_maxcols( DArray *matrix )
 {
     int i;
     int max = 0;
     DArray *record;
 
-    for (i = 0; i < DArray_count(master); i++) {
-        record = (DArray *)(master->contents[i]);
+    for (i = 0; i < DArray_count(matrix); i++) {
+        record = (DArray *)(matrix->contents[i]);
         if ( DArray_count(record) > max ) {
             max = DArray_count(record);
         }
@@ -157,7 +172,7 @@ int DArray_maxcols( DArray *master )
 }
 
 /* Transpose the 2-D array and return another 2-D array */
-DArray *transpose( DArray *master )
+DArray *transpose( DArray *matrix )
 {
     int i, j;
     DArray *xpose  = DArray_create(sizeof(DArray *), 10);
@@ -172,9 +187,9 @@ DArray *transpose( DArray *master )
     DArray_push(xhead, copy);
 
     /* The Header */
-    for (i = 0; i < DArray_count(master) - 1; i++) {
+    for (i = 0; i < DArray_count(matrix) - 1; i++) {
 
-        record = (DArray *)(master->contents[i + 1]);
+        record = (DArray *)(matrix->contents[i + 1]);
 
         if ( DArray_end(record) > 0 ) {
             char val[INT_SIZE + 7];
@@ -186,13 +201,13 @@ DArray *transpose( DArray *master )
 
     DArray_push(xpose, xhead);
 
-    for (j = 0; j < DArray_maxcols(master); j++) {
+    for (j = 0; j < DArray_maxcols(matrix); j++) {
 
         DArray *xrec = DArray_create(sizeof(char *), 10);
 
-        for (i = 0; i < DArray_count(master); i++) {
+        for (i = 0; i < DArray_count(matrix); i++) {
 
-            record = (DArray *)(master->contents[i]);
+            record = (DArray *)(matrix->contents[i]);
 
             if ( DArray_end(record) > 0 ) {
 
@@ -209,27 +224,23 @@ DArray *transpose( DArray *master )
                 else {
                     DArray_push(xrec, (char *)(record->contents[j]));
                 }
-
             }
-
         }
-        
         DArray_push(xpose, xrec);
     }
-
     return xpose;
 }
 
 /* Simply print the 2-D array */
-void tb_print( DArray *master )
+void tb_print( DArray *matrix )
 {
     int i, j, col;
     DArray *record;
     setlocale(LC_CTYPE, "");  // Avoid printing blank
 
-    for (i = 0; i < DArray_count(master); i++) {
+    for (i = 0; i < DArray_count(matrix); i++) {
 
-        record = (DArray *)(master->contents[i]);
+        record = (DArray *)(matrix->contents[i]);
         debug("row %d: ", i + 1);
 
         col = 0;
@@ -242,12 +253,11 @@ void tb_print( DArray *master )
             printf("%s", (char *)(record->contents[j]));
         }
         printf("\n");
-
     }
 }
 
 /* Process a regular delimited file */
-int file_load(char *filename, DArray *master)
+int file_load(char *filename, DArray *matrix)
 {
     char *line = NULL;
     FILE *fp = NULL;
@@ -277,12 +287,12 @@ int file_load(char *filename, DArray *master)
             debug("Processing >>%s<<\n", cell);
             p = strsep (&line, delim);
         }
-        DArray_push(master, record);
-        debug("Loaded record: %d\n", DArray_count(master));
+        DArray_push(matrix, record);
+        debug("Loaded record: %d\n", DArray_count(matrix));
 
     }
 
-    debug("Read a total of %d records\n", DArray_count(master));
+    debug("Read a total of %d records\n", DArray_count(matrix));
 
     if ( line != NULL )
         free(line);
@@ -295,7 +305,7 @@ error:
 }
 
 /* Process a regular delimited file with multi-char delims*/
-int file_load_multi(char *filename, DArray *master)
+int file_load_multi(char *filename, DArray *matrix)
 {
     char *line = NULL;
     char *orig_line = NULL;
@@ -338,16 +348,16 @@ int file_load_multi(char *filename, DArray *master)
         DArray_push(record, cell);
         debug("Processing >>%s<<\n", cell);
 
-        // Now put the whole record on the master array
-        DArray_push(master, record);
-        debug("Loaded record: %d\n", DArray_count(master));
+        // Now put the whole record on the matrix array
+        DArray_push(matrix, record);
+        debug("Loaded record: %d\n", DArray_count(matrix));
         
         if ( orig_line != NULL )
             free(orig_line);
         line = NULL;
     }
 
-    debug("Read a total of %d records\n", DArray_count(master));
+    debug("Read a total of %d records\n", DArray_count(matrix));
 
     fclose(fp);
 
@@ -360,8 +370,8 @@ error:
 // Callback 1 for CSV support, called whenever a field is processed:
 void cb1_x (void *s, size_t len, void *data)
 {
-    DArray *master = (DArray *)data;
-    int last = DArray_end(master) - 1;
+    DArray *matrix = (DArray *)data;
+    int last = DArray_end(matrix) - 1;
 
     if ( (last == -1 && current_record == NULL) || just_pushed_record ) {
         current_record = DArray_create(sizeof(char *), 10);
@@ -381,8 +391,8 @@ void cb2_x (int c, void *data)
     ignore_this = c;
 }
 
-/* Process a CSV file */
-int file_load_csv(char *filename, DArray *master)
+/* Load a CSV file into a dynamic matrix */
+int file_load_csv(char *filename, DArray *matrix)
 {
     struct csv_parser p;
     char buf[1024];
@@ -405,11 +415,11 @@ int file_load_csv(char *filename, DArray *master)
     debug("ready to read some CSV data");
 
     while ((bytes_read=fread(buf, 1, 1024, fp)) > 0) {
-        check(csv_parse(&p, buf, bytes_read, cb1_x, cb2_x, master) == bytes_read
+        check(csv_parse(&p, buf, bytes_read, cb1_x, cb2_x, matrix) == bytes_read
                 , "Error while parsing file: %s", csv_strerror(csv_error(&p)));
     }
 
-    check(csv_fini(&p, cb1, cb2, master) == 0, "Error finishing CSV processing.");
+    check(csv_fini(&p, cb1, cb2, matrix) == 0, "Error finishing CSV processing.");
 
     csv_free(&p);
     fclose(fp);
@@ -489,6 +499,7 @@ int print_table_from_file(char *filename, ft_table_t *table)
     size_t len = 0;           // allocated size for line
     ssize_t bytes_read = 0;   // num of chars read
     setlocale(LC_CTYPE, "");  // Avoid printing blank
+    int line_num = 0;
 
     if (filename[0] == '-') {
         fp = stdin;
@@ -501,18 +512,28 @@ int print_table_from_file(char *filename, ft_table_t *table)
 
     while ((bytes_read = getline(&line, &len, fp)) != -1) {
 
+        line_num++;
         chomp(line);
 
-        char *p = strsep (&line, delim);
-        while (p != NULL)
-        {
-            wchar_t w[strlen(p)+1];              /* VLA */
-            swprintf(w, strlen(p)+1, L"%s", p);  /* Convert char to wchar_t */
-            ft_wwrite(table, w);                 /* Put w in the heap */
-            p = strsep (&line, delim);
-        }
+        debug("line=%d, full=%d, min=%d, max=%d", line_num, full_mode, line_arg, line_arg + row_arg - 1);
 
-        ft_ln(table);
+        if ( full_mode || ((line_num >= line_arg) && (line_num <= line_arg + row_arg - 1)) ) {
+
+            char *p = strsep (&line, delim);
+            while (p != NULL)
+            {
+                wchar_t w[strlen(p)+1];              /* VLA */
+                swprintf(w, strlen(p)+1, L"%s", p);  /* Convert char to wchar_t */
+                ft_wwrite(table, w);                 /* Put w on the heap */
+                p = strsep (&line, delim);
+            }
+
+            ft_ln(table);
+
+        }
+        else if ( line_num > line_arg + row_arg - 1 ) {
+            break;
+        }
 
     }
 
@@ -536,6 +557,7 @@ int print_table_from_file_multi(char *filename, ft_table_t *table)
     size_t len = 0;           // allocated size for line
     ssize_t bytes_read = 0;   // num of chars read
     setlocale(LC_CTYPE, "");  // Avoid printing blank
+    int line_num = 0;
 
     if (filename[0] == '-') {
         fp = stdin;
@@ -548,30 +570,37 @@ int print_table_from_file_multi(char *filename, ft_table_t *table)
 
     while ((bytes_read = getline(&line, &len, fp)) != -1) {
 
+        line_num++;
         chomp(line);
 
-        // Search for tokens using strstr()
-        char *p = strstr(line, delim);
-        while ( p != NULL )
-        {
-            if (p) 
-              *p = '\0';
+        if ( full_mode || ((line_num >= line_arg) && (line_num <= line_arg + row_arg - 1)) ) {
 
+            // Search for tokens using strstr()
+            char *p = strstr(line, delim);
+            while ( p != NULL )
+            {
+                if (p) 
+                  *p = '\0';
+
+                debug("TOKEN: %s", line);
+                wchar_t w[strlen(line)+1];                 /* VLA */
+                swprintf(w, strlen(line)+1, L"%s", line);  /* Convert char to wchar_t */
+                ft_wwrite(table, w);                       /* Put w on the heap */
+
+                line = p + delim_len;
+                p = strstr(line, delim);
+            }
+            // do one more (past the last delimiter):
             debug("TOKEN: %s", line);
             wchar_t w[strlen(line)+1];                 /* VLA */
             swprintf(w, strlen(line)+1, L"%s", line);  /* Convert char to wchar_t */
-            ft_wwrite(table, w);                       /* Put w in the heap */
-
-            line = p + delim_len;
-            p = strstr(line, delim);
+            ft_wwrite(table, w);                       /* Put w on the heap */
+            
+            ft_ln(table);
         }
-        // do one more (past the last delimiter):
-        debug("TOKEN: %s", line);
-        wchar_t w[strlen(line)+1];                 /* VLA */
-        swprintf(w, strlen(line)+1, L"%s", line);  /* Convert char to wchar_t */
-        ft_wwrite(table, w);                       /* Put w in the heap */
-        
-        ft_ln(table);
+        else if ( line_num > line_arg + row_arg - 1 ) {
+            break;
+        }
     }
 
     printf("%ls\n", ft_to_wstring(table));
@@ -594,13 +623,17 @@ int main (int argc, char *argv[])
     int csv_mode = 0;
     int delim_arg_flag = 0;
     int xpose_arg      = 0;
+    int line_arg_flag  = 0;
+    int row_arg_flag   = 0;
+    int add_header_arg = 1;
 
+    /* Loop through the incoming command-line arguments. */
     while (1) {
 
         // getopt_long stores the option index here.
         int option_index = 0;
 
-        c = getopt_long (argc, argv, "hxCd:Q:", long_options, &option_index);
+        c = getopt_long (argc, argv, "AFHhxCd:Q:l:r:", long_options, &option_index);
 
         // Detect the end of the options.
         if (c == -1) break;
@@ -616,9 +649,36 @@ int main (int argc, char *argv[])
                 delim_arg_flag = 1;
                 break;
 
+            case 'l':
+                debug("option -l with value `%s'", optarg);
+                line_arg = atoi(optarg);
+                line_arg_flag = 1;
+                break;
+
+            case 'r':
+                debug("option -r with value `%s'", optarg);
+                row_arg = atoi(optarg);
+                row_arg_flag = 1;
+                break;
+
             case 'x':
                 debug("option -x");
                 xpose_arg = 1;
+                break;
+
+            case 'A':
+                debug("option -A");
+                add_header_arg = 1;
+                break;
+
+            case 'F':
+                debug("option -F");
+                full_mode = 1;
+                break;
+
+            case 'H':
+                debug("option -H");
+                header_arg = 1;
                 break;
 
             case 'h':
@@ -650,6 +710,39 @@ int main (int argc, char *argv[])
 
             default:
                 usage(1);
+        }
+    }
+    
+
+    /*
+     * Notes:
+     *
+     * - When transposing:
+     *   - Option -H defaults to ON
+     *   - Option -l defaults to 1
+     *   - Option -r defaults to 1
+     *   - The net default effect is to output the first row in the file (the header)
+     *
+     * - When NOT transposing:
+     *   - Option -F defaults to ON
+     *     - This causes -l and -r to be ignored
+     *   - The net default effect is to output the entire file.
+     *
+     */
+
+    if ( xpose_arg ) {
+        if ( full_mode == -1 ) {
+            full_mode = 0;
+        }
+    }
+    else {
+        if ( full_mode == -1 ) {
+            if ( line_arg_flag || row_arg_flag ) {
+                full_mode = 0;
+            }
+            else {
+                full_mode = 1;
+            }
         }
     }
 
@@ -685,33 +778,34 @@ int main (int argc, char *argv[])
         debug("The input filename is %s\n", filename);
 
 
-        // If we are transposing:
+        // If we are transposing, load the file into a dynamic 2D array 
+        // first, then transpose it, and print it:
         if (xpose_arg) {
-            DArray *master = DArray_create(sizeof(DArray *), 10);
+            DArray *matrix = DArray_create(sizeof(DArray *), 10);
             if (csv_mode) {
                 debug("Selected CSV mode");
-                check(file_load_csv(filename, master) == 0, "Error loading csv file: %s", filename);
+                check(file_load_csv(filename, matrix) == 0, "Error loading csv file: %s", filename);
             }
             else {
                 debug("Selected DELIM mode");
                 if ( delim_len > 1 ) {
-                    check(file_load_multi(filename, master) == 0, "Error loading delimited file: %s", filename);
+                    check(file_load_multi(filename, matrix) == 0, "Error loading delimited file: %s", filename);
                     debug("Completed file_load_multi()");
                 }
                 else {
-                    check(file_load(filename, master) == 0, "Error loading delimited file: %s", filename);
+                    check(file_load(filename, matrix) == 0, "Error loading delimited file: %s", filename);
                 }
             }
 
-            DArray *xpose = transpose(master);
+            DArray *xpose = transpose(matrix);
             check( xpose != NULL, "Error transposing file: %s", filename);
 
             print_table_from_matrix(xpose);
 
-            master_destroy(master);
+            master_destroy(matrix);
             master_destroy(xpose);
         }
-        // Otherwise, don't load, go from file to table:
+        // Otherwise, don't load to a matrix, go from file to table:
         else {
             ft_table_t *table = ft_create_table();
             ft_set_cell_prop(table, 0, FT_ANY_COLUMN, FT_CPROP_ROW_TYPE, FT_ROW_HEADER);
